c464334fdd6f5c6991026d340cb9d542002ddd6e
granpawalton <36310010+granpawalton@users.noreply.github.com>
2021-01-11 19:28:10 -0600

    init

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 import java.sql._
-
+import scala.collection.mutable.ListBuffer
 object Main extends App {
   var con: Connection = _
 
@@ -9,13 +9,29 @@ object Main extends App {
     con = DriverManager.getConnection(connectionString, "", "")
 
     val statement = con.createStatement()
-    var resultSet = statement.executeQuery("select * from pokes")
+    var resultSet = statement.executeQuery("select * from viewcount")
+
+    var viewCountList = ListBuffer[Tuple2[String, Int]]()
 
     while (resultSet.next) {
-      val foo = resultSet.getString("foo")
-      val bar = resultSet.getString("bar")
-      println(foo + " " + bar)
-    }    
+      val nameString = resultSet.getString("name")
+      val split = nameString.split("\t")
+      viewCountList = viewCountList :+ ((split(0), split(1).toInt))
+    }
+    viewCountList = viewCountList.sortWith(_._2 > _._2)
+    println("TOP TEN VIEWED WIKIPEDIA PAGES:")
+    println("===============================")
+    println(viewCountList(0))
+    println(viewCountList(1))
+    println(viewCountList(2))
+    println(viewCountList(3))
+    println(viewCountList(4))
+    println(viewCountList(5))
+    println(viewCountList(6))
+    println(viewCountList(7))
+    println(viewCountList(8))
+    println(viewCountList(9))
+    println("===============================")
   } catch {
     case e: Exception => e.printStackTrace()
   } 
